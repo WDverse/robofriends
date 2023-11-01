b0b738ad9c96a81a59dd490bec3249dd1700ec6c
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-11-01 17:39:45 -0400

    added feature to filter robots based on search input, changed backgroud color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import  CardList from "./components/CardList";
+import React, { Component } from "react";
+import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-   <div>
-    <CardList robots={robots}/>
-   </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchFiled: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchFiled: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchFiled.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;

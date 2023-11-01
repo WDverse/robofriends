fda713c589c5dbb2ebb027e20761935d17ad218a
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-10-31 23:19:37 -0400

    installed tachyons, deleted some files and completed card component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/Card";
+import { robots } from "./robots";
+
+const App = () => {
+  return (
+    <div className="">
+      <Card  id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Kofi Bredu",
+    username: "Kredu",
+    email: "Kincere@april.bred",
+  },
+  {
+    id: 2,
+    name: "Yaw Wireko",
+    username: "Yariko",
+    email: "ya@wire.ko",
+  },
+  {
+    id: 3,
+    name: "Frank Chap",
+    username: "Frankie",
+    email: "fran@chappy.net",
+  },
+  {
+    id: 4,
+    name: "Peter Lebough",
+    username: "Plebough",
+    email: "pete@gh.ca",
+  },
+  {
+    id: 5,
+    name: "Roland Yzway",
+    username: "Rolway",
+    email: "rollaway@yeezy.com",
+  },
+  {
+    id: 6,
+    name: "Ryan Bonful",
+    username: "Ronful",
+    email: "ronnybon@ai.info",
+  },
+  {
+    id: 7,
+    name: "Kyle Brutis",
+    username: "Le Brute",
+    email: "bigbrute@forte.biz",
+  },
+  {
+    id: 8,
+    name: "Nyarko Baah",
+    username: "Baako",
+    email: "kayuno@wit.me",
+  },
+  {
+    id: 9,
+    name: "Raymond Nortey",
+    username: "Renorlt",
+    email: "rainbolt@tey.io",
+  },
+  {
+    id: 10,
+    name: "Tess Donne",
+    username: "Donne Tessme",
+    email: "tessme@dont.uk",
+  },
+];

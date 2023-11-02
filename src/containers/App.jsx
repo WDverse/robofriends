351452699b709fa0224553fbefdce61db753efad
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-11-02 01:50:43 -0400

    refactored components to use react hooks, cleaned code

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,53 +1,45 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "./ErrorBoundry";
 import "../styles/App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchFiled: "",
-    };
-  }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchFiled, setSearchField] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((res) => {
         return res.json();
       })
       .then((users) => {
-        this.setState({ robots: users });
+        setRobots(users);
       });
-  }
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchFiled: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const { robots, searchFiled } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchFiled.toLowerCase());
-    });
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchFiled.toLowerCase());
+  });
 
-    return !robots.length ? (
-      <h1>Loading...</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
-}
+  return !robots.length ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
--- src/containers/ErrorBoundry.jsx
+++ src/containers/ErrorBoundry.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class ErrorBoundary extends Component {
-  constructor() {
-    super();
-    this.state = {
-      hasError: false,
+const ErrorBoundary = ({ children }) => {
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const handleError = (error, info) => {
+      setHasError(true);
     };
-  }
 
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-  }
+    // Add an event listener for unhandled errors
+    window.addEventListener("error", handleError);
 
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong</h1>;
-    }
-    return this.props.children;
-  }
-}
+    // Return a cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("error", handleError);
+    };
+  }, []);
+
+  return hasError ? <h1>Something went wrong</h1> : children;
+};
 
 export default ErrorBoundary;
